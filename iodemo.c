8ae667940093a749777ee34814cfebd38192e098
malilishadddy <malilishaddy@gmail.com>
2024-07-19 08:24:28 +0300

    My First commit

---
+++ iodemo.c
@@ -0,0 +1,6 @@
+	#include <stdio.h>
+
+	int main(void){
+		printf("My initials are:%c%c%c and my age is %d./n",'S','J','O',25);
+		return(0);
+	}
---
+++ while.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+	int main(){
+		 int a =1;
+
+	while(a<=5){
+		printf("Hello World\n");
+		a++;
+
+	}
+	printf("End of loop");
+	return 0;
+	}
